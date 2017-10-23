ad4297bec403cfd0d88537c6c252e515355eb02b
Jaymee Liu <jaymeeliu@Jaymees-MacBook-Pro.local>
2017-10-22 17:37:51 -0700

    First try

---
+++ keys.js
@@ -0,0 +1,18 @@
+// console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: "XCOz3LoDrAMP7WBTcIiQm2MdS",
+  consumer_secret: "BSHcv2SuoFe70RpN4PBP4Nx30hluBZXvVq7HQiySL9OY0PQkNw",
+  access_token_key: "600329645-XJqZxMFCUcjkMKRlCa9aHtQi0H9Lv5DoWyinTcRQ",
+  access_token_secret: "WwfAfDHRUchCnm8AZFGeMzlHjnNoZ1LV2IowGShI93XWu",
+}
+
+module.exports = twitterKeys;
+
+
+var spotiyKeys = {
+  client_id: "529a090cc17d43538361c13aa755ee7e",
+  client_secert: "0581f6267bbc4c35b28cec44df2080cf"
+}
+
+module.exports = spotiyKeys;
---
+++ liri.js
@@ -0,0 +1,142 @@
+//Liri script
+var keys = require("./keys.js");
+
+//npm packages dependencies and files
+var twitter = require("twitter")
+var spotify = require('spotify');
+var fs = require('fs'); //read and write file
+var request = require('request'); // allows for requests to APIs
+
+//take in command for what does user wants to do? (tweet, moveie, or song search)
+var userCommand = process.argv[2]
+//user input name of song, movie or tweet title
+var lookUpTitle = process.argv[3]
+
+
+//Switch statements to declare what action to excute
+function switchCommand() {
+  switch (userCommand) {
+    case "my-tweets":
+      twitterGrab();
+      break;
+
+    case "spotify-this-song":
+      spotifyGrab();
+      break;
+
+    case "movie-this":
+      ombdGrab();
+      break;
+
+    case "do-what-it-says":
+      doIt();
+      break;
+
+  }
+}; //end of switch function
+
+
+function twitterGrab() {
+  console.log("My tweets:");
+  var client = new twitter(keys.twitterKeys);
+  var parameters = {
+    screen_name: "JayJLiu",
+    count: 20
+  };
+  //call the get method on our client variable twitter instance
+  client.get('statuses/user_timeline', parameters, function(error, tweets, response) {
+    if (!error) {
+      for (i = 0; i < tweets.length; i++) {
+        var returnedData = ('Number: ' + (i + 1) + '\n' + tweets[i].created_at + '\n' + tweets[i].text + '\n');
+        console.log(returnedData);
+        console.log("-------------------------");
+      }
+    };
+  });
+}; //end tweetGrab;
+
+
+
+function ombdGrab() {
+  console.log("Is this the movie you are you looking for?");
+
+  //same as above, test if search term entered
+  var movieSearch;
+  if (lookUpTitle === undefined) {
+    movieSearch = "The Matrix";
+  } else {
+    movieSearch = lookUpTitle;
+  };
+
+  //store ombd request in a variable
+  var ombdURL = 'http://www.omdbapi.com/?t=' + movieSearch + '&y=&plot=long&apikey=40e9cece';
+
+  request(ombdURL, function(error, response, body) {
+    // If the request is successful and not a error
+    if (!error && response.statusCode == 200) {
+      console.log("Title: " + JSON.parse(body)["Title"]);
+      console.log("Year: " + JSON.parse(body)["Year"]);
+      console.log("IMDB Rating: " + JSON.parse(body)["imdbRating"]);
+      console.log("Country: " + JSON.parse(body)["Country"]);
+      console.log("Language: " + JSON.parse(body)["Language"]);
+      console.log("Plot: " + JSON.parse(body)["Plot"]);
+      console.log("Actors: " + JSON.parse(body)["Actors"]);
+      console.log("Rotten Tomatoes Rating: " + JSON.parse(body)["tomatoRating"]);
+      console.log("Rotten Tomatoes URL: " + JSON.parse(body)["tomatoURL"]);
+    }
+  });
+}; //end of movieGrab
+
+
+function spotifyGrab() {
+  console.log("Music time!");
+  //variable for search term, test if defined.
+  var musicSearch;
+  if (lookUpTitle === undefined) {
+    musicSearch = "Young Dumb & Broke";
+  } else {
+    musicSearch = lookUpTitle;
+  }
+  //launch spotify search
+  spotify.search({
+    type: 'track',
+    query: musicSearch
+  }, function(err, data) {
+    if (err) {
+      console.log('Error occurred: ' + err);
+      return;
+    } else {
+      console.log("Artist: " + data.tracks.items[0].artists[0].name);
+      console.log("Song: " + data.tracks.items[0].name);
+      console.log("Album: " + data.tracks.items[0].album.name);
+      console.log("Preview Here: " + data.tracks.items[0].preview_url);
+    }
+  });
+}; //end spotifyGrab
+
+
+function doIt(){
+	console.log("Searching random.txt now");
+	fs.readFile("random.txt", "utf8", function(error, data) {
+	    if(error){
+     		console.log(error);
+     	}else{
+
+     	//split data, declare variables
+     	var dataArr = data.split(',');
+        userCommand = dataArr[0];
+        lookUpTitle = dataArr[1];
+        //if multi-word search term, add.
+        for(i=2; i<dataArr.length; i++){
+            secondCommand = lookUpTitle + "+" + dataArr[i];
+        };
+        //run action
+		switchCommand();
+
+    	};//end else
+
+    });//end readfile
+
+};//end followTheTextbook
+
+switchCommand(); //evoke switchCommand
